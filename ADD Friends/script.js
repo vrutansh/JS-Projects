c3daf5ee0f92b94cac03f8e9649db33d1e1d0f50
Shashank2823 <141436357+Shashank2823@users.noreply.github.com>
2023-10-13 18:19:21 +0530

    Update script.js
    there was a mistake in add.innerHTML because of this add friend not working properly.

--- ADD Friends/script.js
+++ ADD Friends/script.js
@@ -6,13 +6,13 @@ add.addEventListener("click", function(){
     if (flag == 0) {
      a.innerHTML = "Friends";
      a.style.color = "green";
-     btn.innerHTML = "Remove Friend";
+     add.innerHTML = "Remove Friend";
       flag = 1;
     }
     else{
         a.innerHTML = "Removed";
         a.style.color = "red";
-        btn.innerHTML = "Add Friend";
+        add.innerHTML = "Add Friend";
         flag = 0;    
     }
 })
